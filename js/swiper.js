3e713dd0d1bea09d12981961b6350a5d481a973b
Artem <aklimichevm@yandex.ru>
2025-09-23 23:41:01 +0300

    swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,35 @@
-const swiper = new Swiper(".swiper", {
+const heroSwiper = new Swiper(".hero__swiper", {
   loop: true,
   slidesPerView: 1,
   effect: "fade",
   autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
-    speed: 1000,
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  speed: 1000,
+});
+
+
+const plansSwiper = new Swiper(".plans__swiper", {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 0,
+  watchSlidesProgress: true,
+  initialSlide: 1,
+  on: {
+    progress(swiper) {
+      swiper.slides.forEach((slide) => {
+        const slideProgress = slide.progress;
+        const scale = 1 - Math.min(Math.abs(slideProgress * 0.2), 0.4);
+        const opacity = 1 - Math.min(Math.abs(slideProgress / 2), 0.5);
+        slide.style.transform = `scale(${scale})`;
+        slide.style.opacity = opacity;
+      });
+    },
+    setTransition(swiper, transition) {
+      swiper.slides.forEach((slide) => {
+        slide.style.transition = `${transition}ms`;
+      });
+    },
+  },
 });
