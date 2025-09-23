c939870afe6561ec2802e9a6a3dbb29daf3ff8e8
Artem <aklimichevm@yandex.ru>
2025-09-23 15:41:34 +0300

    swiper

---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: 1,
+  effect: "fade",
+  autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+    speed: 1000,
+});
