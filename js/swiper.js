952b0c6d7ca4ef437e1f6acf758bf78fe648329f
Artem <aklimichevm@yandex.ru>
2025-09-24 17:12:04 +0300

    fix

--- js/swiper.js
+++ js/swiper.js
@@ -2,8 +2,13 @@ const heroSwiper = new Swiper(".hero__swiper", {
   loop: true,
   slidesPerView: 1,
   effect: "fade",
-  
+  autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
   speed: 1000,
+  delay: 3000,
+  disableOnInteraction: false,
 });
 
 
