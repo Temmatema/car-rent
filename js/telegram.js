ea0afe0922ac87bd0f0a106a7485ef938a5a0cc0
Artem <aklimichevm@yandex.ru>
2025-10-05 00:58:34 +0300

    fix

--- js/telegram.js
+++ js/telegram.js
@@ -1,27 +1,35 @@
-import { showModalMessage } from "./modal-message.js";
 import { validateForm } from "./form.js";
+import { showModalMessage } from "./modal-message.js";
 
-document.getElementById("rentForm").addEventListener("submit", function (e) {
+const form = document.getElementById("rentForm");
+
+let lastSubmit = 0; // время последней отправки
+
+form.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    if (!validateForm()) {
+    // проверка на частоту
+    const now = Date.now();
+    if (now - lastSubmit < 15000) { // 10 секунд = 10000 мс
+        showModalMessage("error", "Слишком часто", "Пожалуйста, подождите 15 секунд перед повторной отправкой");
         return;
     }
 
-    let name = document.getElementById("name").value;
-    let birthday = document.getElementById("birthday").value;
-    let license = document.getElementById("license").value;
-    let date_license = document.getElementById("date_license").value;
-    let phone = document.getElementById("phone").value;
+    // валидация
+    if (!validateForm()) return;
 
-    let message = `
-🚗 Новая заявка на аренду авто!
-👤 ФИО: ${name}
-📅 Дата рождения: ${birthday}
-🪪 В/У: ${license} (выдано: ${date_license})
-📱 Телефон: ${phone}
-    `;
+    lastSubmit = now; // обновляем время отправки
 
+    // данные
+    let data = {
+        name: document.getElementById("name").value,
+        birthday: document.getElementById("birthday").value,
+        license: document.getElementById("license").value,
+        date_license: document.getElementById("date_license").value,
+        phone: document.getElementById("phone").value,
+    };
+
+    // твой токен и чат
     let token = "8261266499:AAEJkXnAW2sVuGK87PY-LtPuSWAgMXFmw1U";
     let chat_id = "429539553";
 
@@ -30,7 +38,13 @@ document.getElementById("rentForm").addEventListener("submit", function (e) {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
             chat_id: chat_id,
-            text: message,
+            text: `
+🚗 Новая заявка на аренду авто!
+👤 ФИО: ${data.name}
+📅 Дата рождения: ${data.birthday}
+🪪 В/У: ${data.license} (выдано: ${data.date_license})
+📱 Телефон: ${data.phone}
+            `,
             parse_mode: "HTML",
         }),
     })
@@ -39,7 +53,7 @@ document.getElementById("rentForm").addEventListener("submit", function (e) {
             showModalMessage("success", "Заявка отправлена!", "Мы скоро с вами свяжемся");
         })
         .catch((err) => {
-            showModalMessage("error", "Ошибка при отправке!", "Пожалуйста, попробуйте снова");
+            showModalMessage("error", "Ошибка при отправке!", "Попробуйте снова");
             console.error(err);
         });
 });
